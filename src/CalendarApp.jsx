b9b393c084a2d9d224313ede900351130cca9136
LuisFarfan57 <luise.prado99@hotmail.com>
2023-03-09 22:39:56 -0600

    feat: redux configured

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import { AppRouter } from './router'
+import { store } from './store'
 
 export const CalendarApp = () => {
   return (
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
